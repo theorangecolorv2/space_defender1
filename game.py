f977bb335eb059c92d89e7bd46702907837dbf4e
Misha2024 <155589053+Misha2024@users.noreply.github.com>
2024-01-05 02:51:15 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,15 @@
+import pygame, random
+from spaceship import Spaceship
+# from target import Asteroid
+
+
+class Game:
+    def __init__(self, screen_width, screen_height):
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.spaceship_group = pygame.sprite.GroupSingle()
+        self.spaceship_group.add(Spaceship(self.screen_width, self.screen_height))
+        self.asteroids_group = pygame.sprite.Group()
+
+
+
--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 import sys
-from spaceship import Spaceship
-from bullet import Bullet
 import pygame
+import random
+from game import Game
 
 pygame.init()
 
 screen_width = 750
 screen_height = 700
 
-GREY = (29, 29, 27)
+image_fons = pygame.image.load('graphics/fons.png')
+
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Space Adventure')
 
 clock = pygame.time.Clock()
 
-spaceship = Spaceship(screen_width, screen_height)
-spaceship_group = pygame.sprite.GroupSingle()
-spaceship_group.add(spaceship)
+game = Game(screen_width, screen_height)
 
 
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('graphics/asteroids.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, screen_width - self.rect.width)
+        self.rect.y = -self.rect.height
+        self.speed = random.randint(2, 5)
+
+    def update(self):
+        self.rect.y += self.speed
+
+
+asteroids_group = pygame.sprite.Group()
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    if random.randint(1, 100) == 1:
+        new_asteroid = Asteroid()
+        asteroids_group.add(new_asteroid)
 
-    spaceship_group.update()
+    game.spaceship_group.update()
+    asteroids_group.update()
 
-    screen.fill(GREY)
-    spaceship_group.draw(screen)
-    spaceship_group.sprite.bullets_group.draw(screen)
+    screen.blit(image_fons, (0, 0))
+    game.spaceship_group.draw(screen)
+    game.spaceship_group.sprite.bullets_group.draw(screen)
+    asteroids_group.draw(screen)
 
     pygame.display.update()
     clock.tick(60)
