84b036d1b9c7922afd19ed6d02ed82b1eab624b7
1 <theorrv2@gmail.com>
2024-01-08 16:50:56 +0300

    меню реализовал, теперь оно открывает игру пока все сырое, дизайн и лишние принты нужно сделать так, чтобы меню переставало обрабатываться вовремя игры нужно добавить возможность открыть меню из игры.

---
+++ menu.py
@@ -0,0 +1,32 @@
+import pygame
+
+pygame.init()
+
+ARIAL_50 = pygame.font.SysFont("Arial", 50)
+
+class Menu:
+    def __init__(self):
+        self._option_surfaces = []
+        self._callbacks = []
+        self._current_option_index = 0
+
+    def append_option(self, option, callback):
+        self._option_surfaces.append(ARIAL_50.render(option, True, (255,255,255)))
+        self._callbacks.append(callback)
+
+    def switch(self, direction):
+        self._current_option_index = max(0, min(self._current_option_index + direction, len(self._option_surfaces) - 1))
+
+    def select(self):
+        self._callbacks[self._current_option_index]()
+
+    def draw(self, surf, x, y, option_y_padding):
+        for i, option in enumerate(self._option_surfaces):
+            option_rect = option.get_rect()
+            option_rect.topleft = (x, y + i * option_y_padding)
+            if i == self._current_option_index:
+                pygame.draw.rect(surf, (0, 0, 120), option_rect)
+            else:
+                pygame.draw.rect(surf, (0,0,0), option_rect)
+            surf.blit(option, option_rect)
+
